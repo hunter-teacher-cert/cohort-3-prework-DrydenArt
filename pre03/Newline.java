bc0ef87d5271428872dd41fbfedb80f7beb41cd9
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-02-26 16:48:23 +0000

    added javadoc comments to Newline.java, added PrintTwice.java and PrintTwice.class

--- pre03/Newline.java
+++ pre03/Newline.java
@@ -10,11 +10,23 @@ public class Newline {
     newLine();
 
   }
-  //it starts reading here at main regardless of where it is//
+  /**
+   * It starts reading here at main regardless of where it is placed within the code (not necessarily top to bottom.)
+   */
   public static void main(String [] args){
-    System.out.println("First line.");//executes first//
-    threeLine();//invokes threeLine method, invokes newLine method and executes//
-    System.out.println("Second line.");//comes back to main for this instruction//
+    /**
+     *executes first
+     */
+    System.out.println("First line.");
+     /**
+      *invokes threeLine method, which invokes newLine method and executes (x3)
+      */
+    threeLine();
+    /**
+      *then comes back to main for this instruction:
+      */
+    
+    System.out.println("Second line.");
   
     
   }
