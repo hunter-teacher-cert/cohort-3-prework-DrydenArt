1a28f3626317487a7ace29124f0558dde079a404
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-02-28 01:19:16 +0000

    added Exercise.java and Exercise.class

--- pre05/Distance.java
+++ pre05/Distance.java
@@ -6,7 +6,7 @@ public class Distance {
   public static double distance
           (double x1, double y1, double x2, double y2) {
     double dx = x2 - x1;
-    double dy = y2 - y1;;
+    double dy = y2 - y1;
     double dsquared = dx * dx + dy * dy;
     double result = Math.sqrt(dsquared);
     return result;
