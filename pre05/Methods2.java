05b04abaf9933e172242b481011072e482498f46
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-03-17 17:12:37 +0000

    added function call

--- pre05/Methods2.java
+++ pre05/Methods2.java
@@ -3,27 +3,24 @@ import java.util.*;
 
 public class Methods2 {
 
-  public static boolean isDivisble(int n, int m) {
+  public static boolean isDivisible(int n, int m) {
     boolean flagDiv;
     if (m != 0 && n % m == 0) {
       return flagDiv = true;
   }else{
       return flagDiv = false;
   }
-      System.out.println("Is it divisible? " + flagDiv);
-  }
-
     
-
+    //System.out.println(flagDiv);
+  }
 
 
   public static void main(String[] args) {
-    boolean flagDiv = isDivisible(7, 3);
+    boolean flagDiv = isDivisible(7, 3); 
+      System.out.println("Is it divisible? " +flagDiv);
+    
     //int n = 7;
     //int m = 3;
     //isDivisible(7,3);
-  
-
-    
   }
 }
--- pre06/Loops.java
+++ pre06/Loops.java
@@ -3,41 +3,52 @@ import java.util.*;
 
 public class Loops {
 
-  public static void printRow(int n, int cols) {
-    int i = 1;
-    while (i <= cols) {
-      System.out.printf("%4d", n * i);
-      i = i + 1;
-    }
-    System.out.println();
-  }
+  //public static void printRow(int n, int cols) {
+    //int i = 1;
+    //while (i <= cols) {
+      //System.out.printf("%4d", n * i);
+     // i = i + 1;
+    //}
+   // System.out.println();
+ // }
 
-  public static void printTable2(int rows) {
-    for (int i = 1; i <= rows; i = i + 1) {
-      printRow(i, rows);
-      loop(10);
-    }
-  }
+  //public static void printTable2(int rows) {
+   // for (int i = 1; i <= rows; i = i + 1) {
+     // printRow(i, rows);
+     // loop(10);
+    //}
+  //}
 
 
-  public static void loop(int n) {
-    int i = n;
-    while (i > 1) {
-      System.out.println(i);
-      if (i% 2 == 0) {
-        i = i / 2;
-      }else {
-        i = i + 1;
-      }
-    }
-  }
-  //square root: x1 = (xo + a/xo)/2
+  //public static void loop(int n) {
+    //int i = n;
+   // while (i > 1) {
+     // System.out.println(i);
+      //if (i% 2 == 0) {
+        //i = i / 2;
+     // }else {
+        //i = i + 1;
+      //}
+   // }
+ // }
+  //Exercise 2 - square root: x1 = (xo + a/xo)/2
   public static double squareRoot(double a) {
-    double a = 9;
-    double firstGuess = a/2;
-    double newEstimate = a/2;
-    if (Math.abs(a/2 < .000001);
+    double x0;
+    double x1 = a/2;
+    do { x0 = x1;
+      x1 = (x0 + a/x0) /2;
+      } while (Math.abs(x1 - x0) <= .000001);
+    return x1;
     
+  }
+
+  //Exercise 3 - power
+  public static double power(double x, int n) {
+    double p = 1;
+    for (int 1 = 1; i <= n; i += 1); {
+      p = p * x;
+    }
+  return p;
     
   }
 
@@ -47,5 +58,6 @@ public class Loops {
     System.out.println("from Think Java, 7.9:");
     //printTable2(1);
     squareRoot();
+    power();
   }
 }
