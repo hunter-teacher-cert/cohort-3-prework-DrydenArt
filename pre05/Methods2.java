10afd1d2b6c92a21057efc44ad41107ad62c44f9
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-03-17 19:25:40 +0000

    added ack method to Methods2.java

--- pre05/Methods2.java
+++ pre05/Methods2.java
@@ -3,6 +3,20 @@ import java.util.*;
 
 public class Methods2 {
 
+  public static int ack(int m, int n) {
+    if (m == 0) {
+      return n + 1;
+    }else if (m > 0 && n == 0) {
+      int recurse = ack(m - 1, 1);
+      return recurse;
+      }else if (m > 0 && n > 0) {
+        int recurse = ack(m - 1, ack(m,n - 1));
+        return recurse;
+      }
+    return ack(m, n);
+    }
+  
+  
   public static boolean isTriangle(int a, int b, int c) {
     boolean flagTriangle;
     if (a > b + c || b > a + c || c > a + b){
@@ -11,9 +25,8 @@ public class Methods2 {
   else{
     return flagTriangle = true;
     }
-    //return flagTriangle;
   }
-  
+
 
   public static boolean isDivisible(int n, int m) {
     boolean flagDiv;
@@ -21,18 +34,15 @@ public class Methods2 {
       return flagDiv = true;
   }else{
       return flagDiv = false;
+      } 
   }
-    
-  }
-
 
   public static void main(String[] args) {
+    System.out.println(ack(3, 4));
     boolean flagTriangle = isTriangle(2, 2, 2);
       System.out.println("Is it a triangle? " +flagTriangle);
     boolean flagDiv = isDivisible(7, 3);
       System.out.println("Is it divisible? " +flagDiv);
       
-    
-    
   }
 }
