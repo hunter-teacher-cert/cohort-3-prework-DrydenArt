36162dde34b8dee0e359ae5126db0a29f34e6825
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-03-17 19:19:27 +0000

    added isTriangle method to Methods2.java

--- pre05/Methods2.java
+++ pre05/Methods2.java
@@ -3,6 +3,18 @@ import java.util.*;
 
 public class Methods2 {
 
+  public static boolean isTriangle(int a, int b, int c) {
+    boolean flagTriangle;
+    if (a > b + c || b > a + c || c > a + b){
+        return flagTriangle = false;    
+  }
+  else{
+    return flagTriangle = true;
+    }
+    //return flagTriangle;
+  }
+  
+
   public static boolean isDivisible(int n, int m) {
     boolean flagDiv;
     if (m != 0 && n % m == 0) {
@@ -11,16 +23,16 @@ public class Methods2 {
       return flagDiv = false;
   }
     
-    //System.out.println(flagDiv);
   }
 
 
   public static void main(String[] args) {
-    boolean flagDiv = isDivisible(7, 3); 
+    boolean flagTriangle = isTriangle(2, 2, 2);
+      System.out.println("Is it a triangle? " +flagTriangle);
+    boolean flagDiv = isDivisible(7, 3);
       System.out.println("Is it divisible? " +flagDiv);
+      
     
-    //int n = 7;
-    //int m = 3;
-    //isDivisible(7,3);
+    
   }
 }
