aa216265cd8b3e7a5dd8b9a42e70895b3e315432
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-04-18 00:44:19 +0000

    created Array.java and Array.class, started solving 8.1

---
+++ pre07/Array.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Array {
+
+  public static double powArray(double [] a) {
+    double[] values = new double[size];
+    for (int i = 0; i < a.length; i++){
+      a[i] = Math.pow(a[i], 2.0);
+    }
+  }
+
+  public static int histogram()
+
+
+
+
+
+  public static void main(String[] args) {
+    System.out.println("Exercise 8.1 from Think Java, page 135");
+
+    int[] counts = new int[100];
+    for (int score : scores) {
+      counts[score]++;
+    
+  }
+}
