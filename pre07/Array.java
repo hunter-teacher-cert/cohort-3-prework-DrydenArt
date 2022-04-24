b1c6ff5f4a2f07790717cc2afcb93ad87d3dbc30
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-04-24 16:30:26 +0000

    updated Array.java

--- pre07/Array.java
+++ pre07/Array.java
@@ -3,25 +3,35 @@ import java.util.Arrays;
 
 public class Array {
 
+
   public static double [] powArray(double [] a, double power) {
     double[] values = new double[a.length];
+    double power;
     for (int i = 0; i < a.length; i++){
       values[i] = Math.pow(a[i], power);//p128: squares the double array
     }
     return values;
+    //System.out.println(array[i]);
   }
 
   //public static int histogram()
 
 
-
-
-
   public static void main(String[] args) {
     System.out.println("Exercise 8.1 from Think Java, page 135");
-    double [] values = new double [9];
-    double [] powArray = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
+    //double [] powArray = new double [5];
+    double [] powArray = {1.0, 2.0, 3.0, 4.0, 5.0};
     System.out.println(powArray(values));
+    //System.out.println(powArray[i]);
+    //System.out.println(powArray[values]);
+    //System.out.println(powArray[return]);
+    //powArray[0] = 1.0;
+    //powArray[1] = 2.0;
+    //powArray[2] = 3.0;
+    //powArray[3] = 4.0;
+    //powArray[4] = 5.0;
+    //powArray(array);
+  
 
     //int[] counts = new int[100];
     //for (int score : scores) {
