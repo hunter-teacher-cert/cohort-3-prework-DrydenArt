8dde53fc7a23845621b1edbd4283ee3a8c054334
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-04-24 22:17:12 +0000

    started on 8.1.2 in Array.java

--- pre07/Array.java
+++ pre07/Array.java
@@ -10,14 +10,19 @@ public class Array {
       values[i] = Math.pow(a[i], power);//p128: squares the double array
     }
     return values;
-    //System.out.println(values);
   }
 
-  //public static int histogram()
+  public static int[] histogram(int[] a, int scores){
+    int[] counter = new int[100];
+    for (int score : scores); {
+      counter[score]++;
+    }
+    return counter;
+  }
 
 
   public static void main(String[] args) {
-    System.out.println("Exercise 8.1 from Think Java, page 135");
+    System.out.println("Exercise 8.1.1 from Think Java, page 135");
     double[] a;
     a = new double [5];
     double power = 2.0;
@@ -28,22 +33,18 @@ public class Array {
     a[4] = 5.0;
     System.out.println(Arrays.toString(a));
     powArray(a, 2.0);
-    double[] values;
-    //double[] values = new double[a.length];
-    values = powArray(a, 2.0);
+    double[] values;//this stored the return values, declaring the var
+    values = powArray(a, 2.0);//this is how it is assigned to the method
     System.out.println(Arrays.toString(values));
-    //values = new values [5];
-    //final double[] powArray = new values[5];
-    //System.out.println(Arrays.toString(values));
-    //System.out.println(powArray[i]);
-    //System.out.println(powArray[values]);
-    //System.out.println(powArray[return]);
-    //powArray[0] = 1.0;
-    //powArray[1] = 2.0;
-    //powArray[2] = 3.0;
-    //powArray[3] = 4.0;
-    //powArray[4] = 5.0;
-    //powArray(array);
+    
+    System.out.println("Exercise 8.1.2");
+    int[] a = new int[100];
+    int scores = 0; 
+    int[] counter = histogram(a, 100);
+    int[] element = ;
+    
+    histogram(a, 100);
+    System.out.println(Arrays.toString(counter));
   
 
     //int[] counts = new int[100];
