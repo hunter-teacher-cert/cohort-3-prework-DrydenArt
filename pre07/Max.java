42c33d9a93bdfd86473b3c8bd1c8630320e1ecab
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-04-25 03:42:04 +0000

    modified Max.java

--- pre07/Max.java
+++ pre07/Max.java
@@ -17,15 +17,14 @@ public class Max {
 
   public static void main(String[] args) {
     System.out.println("Exercise 8.4, page 137 of Think Java:");
-    int[] a = new int[5];
+    int[] a = new int[3];
     int index;
     a[0] = 10;
     a[1] = 3;
     a[2] = 16;
-    a[3] = 2;
-    a[4] = 12;
-    System.out.println(Arrays.toString(a));
-    int index = indexOfMax(5);
-    System.out.println(Arrays.toString(index));
+    indexOfMax(a);
+    int index;
+    index = indexOfMax(a);
+    System.out.println(a);
   }
 }
